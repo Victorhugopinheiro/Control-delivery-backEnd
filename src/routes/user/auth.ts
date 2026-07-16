b8ff6d68eb2427bc14732df75edcd9ad0313e410
Victor Hugo Pinheiro Lima <azullvictorhugo10@gmail.com>
2026-07-16 18:21:21 -0300

    register user

--- src/routes/user/auth.ts
+++ src/routes/user/auth.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import LoginController from "../../controller/user/auth/loginController.js";
 import LogoutController from "../../controller/user/auth/logoutController.js";
 import RefreshController from "../../controller/user/auth/refreshController.js";
+import RegisterController from "../../controller/user/auth/registerController.js";
 
 export const authRouter = Router();
 const loginController = new LoginController();
 const refreshController = new RefreshController();
 const logoutController = new LogoutController();
+const registerController = new RegisterController();
 
+authRouter.post("/register", (req, res) => registerController.handle(req, res));
 authRouter.post("/login", (req, res) => loginController.handle(req, res));
 authRouter.post("/refresh", (req, res) => refreshController.handle(req, res));
 authRouter.post("/logout", (req, res) => logoutController.handle(req, res));
